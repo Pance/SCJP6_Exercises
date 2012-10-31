a5384a6ed920cefc7898e4bdec52e75418971955
Peter Barnett <mrpants888@gmail.com>
2012-10-31 16:52:31 -0500

    Added Exercise_5_1

---
+++ Exercise_5_1/switch_statement_with_char.java
@@ -0,0 +1,19 @@
+public class switch_statement_with_char {
+	public static void main(String[] args) {
+		char c = 'c';
+
+		switch(c) {
+			case 'a': 
+				System.out.println("Letter A");
+				break;
+			case 'b': 
+				System.out.println("Letter B");
+				break;
+			case 'c': 
+				System.out.println("Letter C");
+				break;
+			default:
+				System.out.println("Default here");
+		}
+	}
+}
